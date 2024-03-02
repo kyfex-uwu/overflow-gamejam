d2731c5f24533f8b1610c1b6cd3bfaa70db96c4f
kyfex-uwu <retrogrape117@gmail.com>
2024-03-02 14:11:10 -0700

    Create disk.py

---
+++ entity/disk.py
@@ -0,0 +1,12 @@
+import os
+
+import pygame
+
+from entity.entity import Entity
+
+DISK_IMAGE = pygame.image.load(os.path.join('resources', 'entity', 'disk.png')).convert_alpha()
+class DiscEntity(Entity):
+    def __init__(self, x, y):
+        super().__init__(x, y, 16,16)
+
+    def render(self):
