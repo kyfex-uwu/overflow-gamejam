1a20dd71586c2df101a00c171a1823623c494ce2
kyfex-uwu <retrogrape117@gmail.com>
2024-03-02 12:07:57 -0700

    added tiles!

--- entity/tiles.py
+++ entity/tiles.py
@@ -1,13 +1,44 @@
+import os
+import pygame
 from entity.solid import SolidEntity
 
+class TileColl(SolidEntity):
+    def render(self): pass
 
 class Tiles(SolidEntity):
-    def __init__(self, palette, tileStr:str):
+    def __init__(self, palette, tileStr: str):
         super().__init__(0, 0, 0, 0)
         self.palette = palette
 
         rows = tileStr.split("\n")
-        self.tiles = [[None]*len(rows[0]) for _ in rows]
+        self.w = len(sorted(rows, key=lambda row: len(row), reverse=True)[0])
+        self.h = len(rows)
+
+        self.tiles = []
+        self.tileColls = []
+        paletteImages = set()
+        for y in range(self.h):
+            self.tiles.append([])
+            rowLength = len(rows[y])
+            for x in range(self.w):
+                if x >= rowLength or rows[y][x] == ".":
+                    self.tiles[y].append(None)
+                else:
+                    self.tiles[y].append(palette[rows[y][x]])
+                    self.tileColls.append(TileColl(x*8,y*8,8,8))
+                    paletteImages.add(palette[rows[y][x]][2])
+        self.images = {}
+        for image in paletteImages:
+            self.images[image] = pygame.image.load(os.path.join('resources', image + '.png')).convert_alpha()
+
+    def init(self, level):
+        super().init(level)
+        for tileColl in self.tileColls:
+            tileColl.init(level)
 
     def render(self):
-        pass
+        for y in range(self.h):
+            for x in range(self.w):
+                if self.tiles[y][x] is None: continue
+                self.level.surface.blit(self.images[self.tiles[y][x][2]], (x * 8, y * 8),
+                                        (0, 0, 8, 8))
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-
 import pygame
 
 from entity.player import PlayerEntity
 from entity.solid import SolidEntity
+from entity.tiles import Tiles
 from level import Level
 
 # pygame setup
@@ -14,16 +13,24 @@ running = True
 dt = 0
 
 test_level = Level(70,30)
-SolidEntity(24,14.800003051757812,14,23,(50,50,70)).init(test_level)
-SolidEntity(48,16.800003051757812,14,26,(50,50,70)).init(test_level)
-SolidEntity(8,52.80000305175781,10,30,(50,50,70)).init(test_level)
-SolidEntity(19,82.80000305175781,51,13,(50,50,70)).init(test_level)
-SolidEntity(70,53.80000305175781,11,27,(50,50,70)).init(test_level)
-SolidEntity(33,49.80000305175781,20,6,(50,50,70)).init(test_level)
-SolidEntity(36,54.80000305175781,14,5,(50,50,70)).init(test_level)
-SolidEntity(35,-10.199996948242188,0,0,(50,50,70)).init(test_level)
-test_entity2 = PlayerEntity(50, 50, 8,8,(30,40,50))
-test_entity2.init(test_level)
+SolidEntity(9, 6, 6, 4).init(test_level)
+
+PlayerEntity(10,10,10,10).init(test_level)
+
+Tiles({
+    "A": (3,0,"test")
+},"""
+
+
+
+
+
+
+
+
+
+AA.AAAA
+......A""").init(test_level)
 
 while running:
     for event in pygame.event.get():
@@ -37,7 +44,4 @@ while running:
     test_level.render(SCREEN)
     pygame.display.flip()
     dt = clock.tick(60) / 1000
-    test_entity2.rcolor += round(math.sin(test_entity2.bcolor))
-    test_entity2.bcolor += round(math.sin(test_entity2.gcolor))
-    test_entity2.gcolor += round(math.sin(test_entity2.rcolor + 1))
 pygame.quit()
