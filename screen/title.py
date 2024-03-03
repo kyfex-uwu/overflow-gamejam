53e0f8d1489271ac0bc552e4c806c45aa5ba2109
kyfex-uwu <retrogrape117@gmail.com>
2024-03-02 19:44:23 -0700

    screens working
    -fixed disk

--- screen/title.py
+++ screen/title.py
@@ -1,6 +1,11 @@
+import pygame
+from pygame import Surface
+
 from screen.screen import Screen
 
 
 class TitleScreen(Screen):
-    def render(self, screen):
-        pass
+    def render(self, screen: Surface):
+        screen.fill(pygame.Color(30,30,60))
+        screen.blit(pygame.font.SysFont(pygame.font.get_default_font(),32).render(
+            "test", True, (255,255,255)),(10,10))
